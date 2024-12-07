2e57b8b0e1187db8572b887ca15f9c95450f1117
Ashwin D <ashwinthegreat3@gmail.com>
2024-12-07 10:30:03 +0800

    initial commit

---
+++ src/main/java/com/hackathon/uob/MortgageApplication/MortgageApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.uob.MortgageApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgageApplication.class, args);
+	}
+
+}
