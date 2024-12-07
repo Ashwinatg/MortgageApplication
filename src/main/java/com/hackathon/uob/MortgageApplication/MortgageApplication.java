f28fe558fd400116e07c1080333fe0882321d401
Nikhil <nikhilchaurasia99@gmail.com>
2024-12-07 15:33:59 +0800

    Database table creation fixed

--- src/main/java/com/hackathon/uob/MortgageApplication/MortgageApplication.java
+++ src/main/java/com/hackathon/uob/MortgageApplication/MortgageApplication.java
@@ -2,8 +2,14 @@ package com.hackathon.uob.MortgageApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.hackathon.uob.repository")
+@ComponentScan(basePackages = { "com.hackathon.uob.*" })
+@EntityScan("com.hackathon.uob.*")
 public class MortgageApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/hackathon/uob/entity/Transaction.java
+++ src/main/java/com/hackathon/uob/entity/Transaction.java
@@ -1,15 +1,12 @@
 package com.hackathon.uob.entity;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
+@Data
 @Entity
-@Table(name = "transactions")
-@Getter
-@Setter
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
